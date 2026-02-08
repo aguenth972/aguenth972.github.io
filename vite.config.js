668f6fec35e41da61ca903fbfdf74bec12ed4a06
aguenther972 <guenthea@whitman.edu>
2026-02-08 14:33:04 -0800

    Fixed npm build issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // This ensures assets load correctly on your custom domain
   plugins: [
     react(),
     tailwindcss(),
